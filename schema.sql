3edad98e99e4202ad0817f5b89d4b9efb866515c
William Lau <contact@cwlau.com>
2016-08-15 11:01:39 +0800

    Update form and backend api/schema

--- schema.sql
+++ schema.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `contract` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `address` varchar(45) DEFAULT NULL,
+  `contract_name` varchar(45) DEFAULT NULL,
   `nickname` varchar(255) DEFAULT NULL,
   `abi_definition` longtext,
   `creation_datetime` datetime DEFAULT NULL,
